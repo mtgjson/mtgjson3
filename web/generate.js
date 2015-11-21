f5f10120b8430416deae8fcc45a736c25e682f7b
Sergio Moura <sergio@moura.us>
2015-11-21 11:28:25 -0300

    Getting ready for multi-language

--- web/generate.js
+++ web/generate.js
@@ -21,7 +21,8 @@ var dustData =
 {
 	title : "Magic the Gathering card data in JSON format",
 	sets  : [],
-	setCodesNotOnGatherer : C.SETS_NOT_ON_GATHERER.join(", ")
+	setCodesNotOnGatherer : C.SETS_NOT_ON_GATHERER.join(", "),
+	analytics : "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-66983210-2', 'auto');ga('send', 'pageview');</script>";
 };
 
 tiptoe(
