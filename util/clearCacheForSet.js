cc09bf2e1165a8ac3d908d02d45a59fa5f59db75
Sergio Moura <sergio@moura.us>
2016-03-31 16:34:02 -0300

    Refactoring the Clear Cache file

--- util/clearCacheForSet.js
+++ util/clearCacheForSet.js
@@ -6,10 +6,10 @@ var base = require("xbase"),
 	fs = require("fs"),
 	shared = require("shared"),
 	path = require("path"),
+	async = require('async'),
 	tiptoe = require("tiptoe");
 
-if(process.argv.length<4)
-{
+if (process.argv.length < 4) {
 	base.error("Usage: node %s <all|oracle|original|languages|printings|legalities|mcilist|listings> <set codes>", process.argv[1]);
 	process.exit(1);
 }
@@ -17,21 +17,27 @@ if(process.argv.length<4)
 var VALID_TYPES = ["oracle", "original", "languages", "printings", "legalities", "mcilist", "listings"];
 
 var cacheTypes = process.argv[2].toLowerCase()==="all" ? VALID_TYPES : Array.toArray(process.argv[2]);
-cacheTypes.serialForEach(function(cacheType, cb)
-{
-	if(!VALID_TYPES.contains(cacheType))
-	{
-		base.error("Invalid cacheType: %s", cacheType);
-		return;
-	}
 
-	base.info("Clearing cache type: %s", cacheType);
+async.eachSeries(
+	cacheTypes,
+	function(cacheType, cb) {
+		if (!VALID_TYPES.contains(cacheType)) {
+			base.error("Invalid cacheType: %s", cacheType);
+			return;
+		}
 
-	shared.getSetsToDo(3).serialForEach(function(code, subcb) { clearCacheForSet(code, cacheType, subcb); }, cb);
-}, function(err)
-	{
-		if(err)
-		{
+		base.info("Clearing cache type: %s", cacheType);
+
+		async.eachSeries(
+			shared.getSetsToDo(3),
+			function(code, subcb) {
+				clearCacheForSet(code, cacheType, subcb);
+			},
+			cb
+		);
+	},
+	function(err) {
+		if (err) {
 			base.error(err);
 			process.exit(1);
 		}
@@ -40,21 +46,23 @@ cacheTypes.serialForEach(function(cacheType, cb)
 	}
 );
 
-function clearCacheForSet(code, cacheType, cb)
-{
-	var setName = C.SETS.mutateOnce(function(SET) { if(SET.code.toLowerCase()===code.toLowerCase()) { return SET; } }).name;
+function clearCacheForSet(code, cacheType, cb) {
+	var setName = C.SETS.mutateOnce(
+		function(SET) {
+			if (SET.code.toLowerCase() === code.toLowerCase()) {
+				return SET;
+			}
+		}
+	).name;
 
 	tiptoe(
-		function loadSetJSON()
-		{
-			fs.readFile(path.join(__dirname, "..", "json", code + ".json"), {encoding : "utf8"}, this);
+		function loadSetJSON() {
+			fs.readFile(path.join(__dirname, "..", "json", code + ".json"), { encoding : "utf8" }, this);
 		},
-		function getCacheURLS(setRaw)
-		{
+		function getCacheURLS(setRaw) {
 			var set = JSON.parse(setRaw);
-			if(cacheType==="mcilist")
-			{
-				if(!set.isMCISet) {
+			if (cacheType === "mcilist") {
+				if (!set.isMCISet) {
 					base.info("Not MCISet");
 
 					if (!set.magicCardsInfoCode)
@@ -66,10 +74,11 @@ function clearCacheForSet(code, cacheType, cb)
 				urls.push(mciListUrl);
 				var targetSet = C.SETS.mutateOnce(function(SET) { if(SET.code.toLowerCase()===set.code.toLowerCase()) { return SET; } });
 				if (targetSet.magicRaritiesCodes) {
-					targetSet.magicRaritiesCodes.forEach(function(magicRaritiesCode)
-					{
-						urls.push("http://www.magiclibrarities.net/" + magicRaritiesCode + "-english-cards-index.html");
-					}.bind(this));
+					targetSet.magicRaritiesCodes.forEach(
+						function(magicRaritiesCode) {
+							urls.push("http://www.magiclibrarities.net/" + magicRaritiesCode + "-english-cards-index.html");
+						}
+					);
 				}
 
 				// Retrieve all mci info and clear it
@@ -80,7 +89,7 @@ function clearCacheForSet(code, cacheType, cb)
 						base.info('Getting URL Contents for: %s', mciListUrl);
 						shared.getURLAsDoc(mciListUrl, this);
 					},
-					function getURLs(listDoc) {
+					function getURLs(err, listDoc) {
 						var mciCardLinks = Array.toArray(listDoc.querySelectorAll("table tr td a"));
 						var validLinks = mciCardLinks.filter(function(link) {
 							var href = link.getAttribute("href");
@@ -91,36 +100,53 @@ function clearCacheForSet(code, cacheType, cb)
 							urls.push("http://magiccards.info" + href);
 						});
 
-						this();
-					},
-					function finish(err) {
-						cb(err, urls);
+						setImmediate(cb, err, urls);
 					}
 				);
 			}
-			else if(cacheType==="listings")
-			{
+			else if (cacheType === "listings") {
 				return shared.buildMultiverseListingURLs(setName, this);
 			}
-			else
-			{
+			else {
 				base.info("%s: %d cards found.", code, set.cards.length);
-				set.cards.filter(function(card) { return card.hasOwnProperty("multiverseid"); }).serialForEach(function(card, subcb)
-				{
-					shared.buildCacheFileURLs(card, cacheType, subcb, true);
-				}, this);
+				var urls = [];
+				async.eachSeries(
+					set.cards.filter(
+						function(card) {
+							return(card.hasOwnProperty("multiverseid"));
+						}
+					),
+					function(card, subcb) {
+						shared.buildCacheFileURLs(
+							card,
+							cacheType,
+							function(err, url) {
+								urls.push(url);
+								subcb();
+							},
+							true
+						);
+					},
+					function(err) {
+						this(err, urls);
+					}.bind(this)
+				);
 			}
 		},
-		function clearCacheFiles(urls)
-		{
+		function clearCacheFiles(urls) {
+			if (!urls)
+				return(setImmediate(this, new Error("No urls for clearCacheFiles().")));
 			urls = urls.flatten().uniqueBySort();
 
 			base.info("Clearing %d URLS", urls.length);
-			urls.serialForEach(shared.clearCacheFile, this);
+			async.eachSeries(
+				urls,
+				shared.clearCacheFile,
+				this
+			);
 		},
-		function finish(err)
-		{
-			setImmediate(function() { cb(err); });
+		function finish(err) {
+			setImmediate(cb, err);
 		}
 	);
 }
